a3491315d1d1d5e0d1afa041b1ae6cfccdd5e4bd
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2022-09-21 15:43:35 +0300

    updating getRecipes api calls code

--- src/App.js
+++ src/App.js
@@ -7,46 +7,46 @@ import Recipes from "./components/Recipes";
 const API_KEY = "d05e5019b4dc4284a7531d46bdcab182";
 
 class App extends Component {
-  state = {
-    recipes: []
-  };
+	state = {
+		recipes: []
+	};
 
-  getRecipe = async e => {
-    const recipeName = e.target.elements.recipeName.value;
-    e.preventDefault();
-    const api_call = await fetch(
-      `https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=12`
-    );
-    const data = await api_call.json();
-    this.setState({ recipes: data.recipes });
-    console.log(this.state.recipes);
-  };
+	getRecipe = async (e) => {
+		e.preventDefault();
+		const recipeName = e.target.elements.recipeName.value;
+		const api_call = await fetch(
+			`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=12`
+		);
+		const data = await api_call.json();
+		this.setState({ recipes: data.recipes });
+		console.log(this.state.recipes);
+	};
 
-  componentDidMount = () => {
-    if (localStorage.getItem("recipes") != null) {
-      const json = localStorage.getItem("recipes");
-      const recipes = JSON.parse(json);
-      this.setState({ recipes });
-    }
-  };
+	componentDidMount = () => {
+		if (localStorage.getItem("recipes") != null) {
+			const json = localStorage.getItem("recipes");
+			const recipes = JSON.parse(json);
+			this.setState({ recipes });
+		}
+	};
 
-  componentDidUpdate = () => {
-    const recipes = JSON.stringify(this.state.recipes);
-    localStorage.setItem("recipes", recipes);
-  };
+	componentDidUpdate = () => {
+		const recipes = JSON.stringify(this.state.recipes);
+		localStorage.setItem("recipes", recipes);
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Tasty Recipes</h1>
-        </header>
-        <div className="Form__div" />
-        <Form getRecipe={this.getRecipe} />
-        <Recipes recipes={this.state.recipes} />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Tasty Recipes</h1>
+				</header>
+				<div className="Form__div" />
+				<Form getRecipe={this.getRecipe} />
+				<Recipes recipes={this.state.recipes} />
+			</div>
+		);
+	}
 }
 
 export default App;
