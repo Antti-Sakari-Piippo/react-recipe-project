e5cf8bc8345f542c840d6ccd066c051643c603a1
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2022-09-21 15:16:03 +0300

    fix form layout

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Form = props => (
-  <form onSubmit={props.getRecipe} style={{ marginBottom: "2rem" }}>
-    <input className="form__input" type="text" name="recipeName" />
-    <button className="form__button">Search</button>
-  </form>
+const Form = (props) => (
+	<form onSubmit={props.getRecipe}>
+		<label>
+			<input type="text" name="name" />
+		</label>
+		<input className="form__button" type="submit" value="Submit" />
+	</form>
 );
 
 export default Form;
